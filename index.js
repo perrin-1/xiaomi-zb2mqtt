225f0f680d4af88e7b6be09addaa08538d90f1de
perrin-1 <ulv@on2.de>
2018-05-20 12:09:48 +0200

    Update index.js - Xiaomi battery voltage
    Update index.js - added functionality to decode Xiaomi sensors battery voltage. Will report voltage as absolute value (e.g. 3.005v) to MQTT

--- index.js
+++ index.js
@@ -12,6 +12,37 @@ var shepherd = new ZShepherd('/dev/ttyACM0', {
     }
 });
 
+function toUTF8Array(str) {
+    var utf8 = [];
+    for (var i=0; i < str.length; i++) {
+        var charcode = str.charCodeAt(i);
+        if (charcode < 0x80) utf8.push(charcode);
+        else if (charcode < 0x800) {
+            utf8.push(0xc0 | (charcode >> 6), 
+                      0x80 | (charcode & 0x3f));
+        }
+        else if (charcode < 0xd800 || charcode >= 0xe000) {
+            utf8.push(0xe0 | (charcode >> 12), 
+                      0x80 | ((charcode>>6) & 0x3f), 
+                      0x80 | (charcode & 0x3f));
+        }
+        // surrogate pair
+        else {
+            i++;
+            // UTF-16 encodes 0x10000-0x10FFFF by
+            // subtracting 0x10000 and splitting the
+            // 20 bits of 0x0-0xFFFFF into two halves
+            charcode = 0x10000 + (((charcode & 0x3ff)<<10)
+                      | (str.charCodeAt(i) & 0x3ff));
+            utf8.push(0xf0 | (charcode >>18), 
+                      0x80 | ((charcode>>12) & 0x3f), 
+                      0x80 | ((charcode>>6) & 0x3f), 
+                      0x80 | (charcode & 0x3f));
+        }
+    }
+    return utf8;
+}
+
 shepherd.on('ready', function() {
     console.log('Server is ready. Current devices:');
     shepherd.list().forEach(function(dev){
@@ -46,6 +77,15 @@ shepherd.on('ind', function(msg) {
             pl=1;
 
             switch (msg.data.cid) {
+                case 'genBasic': //Basic device information
+                    if(typeof msg.data.data[65281] !== 'undefined') { // decode Xiaomi device string for battery information
+                      var battStr = toUTF8Array(msg.data.data[65281]);
+                      var voltage = (battStr[5] << 8) + battStr[4];
+                      topic += '/battery';
+                      pl = voltage / 1000.0;
+                      console.log ('Voltage '+ pl);
+                    } 
+                    break; 
                 case 'genOnOff':  // various switches
                     topic += '/' + msg.endpoints[0].epId;
                     pl = msg.data.data['onOff'];
